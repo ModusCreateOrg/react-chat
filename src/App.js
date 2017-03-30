638c845accf456e5fe1780aa7f05c99a711b67ed
Grgur Grisogono <grgur@grgur.com>
2017-03-30 15:43:20 +0200

    HTML mockup

--- src/App.js
+++ src/App.js
@@ -8,11 +8,18 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>React Chat</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <main>
+          <ul>
+            <li>Hello John</li>
+            <li>Hi Judy</li>
+          </ul>
+          <div>
+            <input name="user" />
+            <input name="message" />
+          </div>
+        </main>
       </div>
     );
   }
