026af823be852ac88c4cb944ce8dd0f1eb7bfe07
Grgur Grisogono <grgur@grgur.com>
2017-03-30 16:12:01 +0200

    Path resolving

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import MessageList from './components/MessageList';
+import MessageList from 'containers/MessageList';
 import './App.css';
 
 class App extends Component {
